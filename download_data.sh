95092cfa08044258ebc338c91caa5f1e5ed621e4
B-Tulgat <ttulgat@gmail.com>
2025-07-01 15:24:55 +0800

    download shell

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+aws s3 cp  s3://ont-open-data/rna-modbase-validation_2025.03/references/drach_context_strands.fa . --no-sign-request
+aws s3 cp  s3://ont-open-data/rna-modbase-validation_2025.03/subset/m6A_DRACH.pod5 . --no-sign-request
+aws s3 cp  s3://ont-open-data/rna-modbase-validation_2025.03/subset/control_DRACH.pod5 . --no-sign-request
+
